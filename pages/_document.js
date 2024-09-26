52705f8b81179a2e018d637f8ba50aa6fd7c7def
VISHAL DUBEY <DUBEYVISHAL917@GMAIL.COM>
2024-09-26 15:25:53 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
      <meta name="google-site-verification" content="78IKF1o38uymg2UfKIzqDaJX9aoc9cxWEhnRjhJHsKo" />
+    <meta name="msvalidate.01" content="CD37CB6FC57962A460A72499700C0659" />
         {/* Basic Meta Tags */}
         <meta name="description" content="ViShare - Share files securely and quickly via peer-to-peer connections" />
         <link rel="icon" href="/favicon.ico" />
